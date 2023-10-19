8d1eb30dcc0fd2c2e7e4e10a7e2fe12be0cb2a45
Alexandr <sasha-bragin@mail.ru>
2023-10-19 19:26:24 +0300

    comment added

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
     document.addEventListener('DOMContentLoaded', function(){
         document.getElementById('button').addEventListener('click', function(){
             let text = document.getElementById('text').value;
-            alert('Длина строки: ' + text)
-            // some comment
+            alert('Длина строки: ' + text);
+            // some text
         });
     });
 })();
